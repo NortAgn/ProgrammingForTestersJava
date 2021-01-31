b8148a3bc04e7c56996368656e6ebaedd4f0bf8e
Agnia <64902167+NortAgn@users.noreply.github.com>
2021-01-31 23:19:08 +0300

    Задание №2. Вычисление расстояния м/у двумя точками на двумерной плоскости

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,29 +3,25 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
-    hello("Agnia");
-    hello("user");
+  // hello("world");
+  //hello("Agnia");
+  //hello("user");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+  //Square s = new Square(5);
+  // System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  //Rectangle r = new Rectangle(4, 6);
+  //System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+
+    Point p1 = new Point(1, 0.66);
+    Point p2 = new Point(5, -9);
+    System.out.println("Расстояние между точками (" + p1.x + ";" + p1.y + ") и (" + p2.x + ";" + p2.y + ") = " + Point.distance(p1, p2));
+
+    //public static void hello(String somebody) {
+    // System.out.println("Hello," + somebody + "!");
+    //}
+
 
   }
-
-  public static void hello(String somebody) {
-    System.out.println("Hello," + somebody + "!");
-  }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
-
 }
