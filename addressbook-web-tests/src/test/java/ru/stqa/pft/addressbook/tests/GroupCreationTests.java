6125bfbec695af7d34118cbd9c1031c2539213d2
Agnia <64902167+NortAgn@users.noreply.github.com>
2021-02-09 00:26:03 +0300

    Выстроена двухслойная архитектура тестового набора
    теперь есть специализированные классы-помощники

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase{
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("test2", "test4", "test5"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
+    //wd.findElement(By.linkText("Logout")).click();
+  }
+
+}
