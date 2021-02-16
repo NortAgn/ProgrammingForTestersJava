4799d9eefedf57cf3255bd2a1a42fe4b7f715977
Agnia <64902167+NortAgn@users.noreply.github.com>
2021-02-17 00:23:30 +0300

    Оптимизация заполнения форм: оставляем дефолтные значения в полях
    Лекция 3.5. исправленная версия

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,7 +8,7 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test2", "test4", "test5"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test2", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
     //wd.findElement(By.linkText("Logout")).click();
