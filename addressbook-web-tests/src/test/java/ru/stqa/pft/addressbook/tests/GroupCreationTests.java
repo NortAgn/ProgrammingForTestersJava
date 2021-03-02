6b1b921683e29a5200d103dbc4ca63e681051e37
Agnia <64902167+NortAgn@users.noreply.github.com>
2021-03-02 22:29:15 +0300

    в продолжение лекции 5.4
    изменения по группам

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,23 +5,19 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test3", null, null);
-    app.getGroupHelper().createGroup(group);
-    List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(),before.size() +1 );
+    app.goTo().groupPage();
+    List<GroupData> before = app.group().list();
+    GroupData group = new GroupData().withName("test3");
+    app.group().create(group);
+    List<GroupData> after = app.group().list();
+    Assert.assertEquals(after.size(),before.size() + 1 );
 
-
-    //group.setId(after.stream().max((Comparator<GroupData>) (o1, o2) ->
-            //Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(group);
     Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
     before.sort(byId);
